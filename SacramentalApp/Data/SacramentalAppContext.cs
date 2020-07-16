52111a45be7a5383beea1e8299967529b0b6cf43
vera-hugo <ver16004@byui.edu>
2020-07-15 20:37:48 -0500

    Adding Controller, View, and Model

---
+++ SacramentalApp/Data/SacramentalAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SacramentalApp.Models
+{
+    public class SacramentalAppContext : DbContext
+    {
+        public SacramentalAppContext (DbContextOptions<SacramentalAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SacramentalApp.Models.Meeting> Meeting { get; set; }
+    }
+}
---
+++ SacramentalApp/Models/Meeting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SacramentalApp.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+
+        
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [StringLength(100, MinimumLength = 10)]
+        [Display(Name = "Conducting Leader")]
+        public string ConductingLeader { get; set; }
+
+        [StringLength(100, MinimumLength = 10)]
+        [Display(Name = "Opening Song")]        
+        public string OpeningSong { get; set; }
+
+        [StringLength(100, MinimumLength = 10)]
+        [Display(Name = "Opening Prayer")]
+        public string OpeningPrayer { get; set; }
+
+        [StringLength(100, MinimumLength = 10)]
+        [Display(Name = "Sacrament Hymn")]
+        public string SacramentHymn { get; set; }
+
+        [StringLength(100, MinimumLength = 10)]
+        public string Speaker { get; set; }
+
+        [StringLength(100, MinimumLength = 10)]
+        [Display(Name = "Closening Song")]
+        public string CloseningSong { get; set; }
+
+        [StringLength(100, MinimumLength = 10)]
+        [Display(Name = "Closening Prayer")]
+        public string CloseningPrayer { get; set; }
+
+        
+    }
+
+}
